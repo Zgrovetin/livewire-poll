d962df2fdecc74cb8137b2e36d7925173140f952
Zgrovetin <534477@gmail.com>
2024-10-10 11:37:27 +0200

    add / remove on the page only

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -13,9 +13,16 @@
         <div>
             @foreach ($options as $index => $option)
                 <div class="mb-4">
-                    {{ $index }} - {{ $option }}
+{{--                    {{ $index }} - {{ $option }}--}}
+                    <label>Option {{ $index + 1 }}</label>
+
+                    <div class="flex gap-2">
+                        <input type="text" wire:model="options.{{ $index }}"/>
+                        <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                    </div>
                 </div>
+
             @endforeach
         </div>
     </form>
-</form>
+</div>
